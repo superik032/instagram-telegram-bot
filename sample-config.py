f6b42678b0e1a66c67fa9e599c98869938baea4d
superik032 <90202168+superik032@users.noreply.github.com>
2021-09-08 00:35:40 +0500

    Update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -1,9 +1,9 @@
-TOKEN = 'YOUR-BOT-TOKEN'
+TOKEN = '1942590259:AAEPrd-ty7l8Q0dCOjUlGP8SkLpoiFDHqik'
 admin_id = 0
 helper_id = 0
 
 ##### upload_file.py config #####
 api_id = 0
-api_hash = 'api_hash'
+api_hash = '45b52a5afaff024023817a2827b6d077'
 name = 'name'
 bot_username = 'bot_username'
