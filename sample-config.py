9bf53fc818ca88311702e2e952e2cd22dc9d5c06
superik032 <90202168+superik032@users.noreply.github.com>
2021-09-08 00:37:07 +0500

    Update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -5,5 +5,5 @@ helper_id = 0
 ##### upload_file.py config #####
 api_id = 0
 api_hash = '45b52a5afaff024023817a2827b6d077'
-name = 'name'
-bot_username = 'bot_username'
+name = 'InstaBot'
+bot_username = 'instasaveru_bot'
